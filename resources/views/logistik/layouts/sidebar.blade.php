8be57df32177031ec5480512e46ca3d0e304d7ef
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-08-02 04:47:01 +0700

    Upload resources/views/logistik/layouts/sidebar.blade.php

---
+++ resources/views/logistik/layouts/sidebar.blade.php
@@ -0,0 +1,34 @@
+<li class="pc-item pc-caption">
+  <label data-i18n="Navigasi">Navigasi</label>
+  <i class="ph-duotone ph-gauge"></i>
+</li>
+<li class="pc-item">
+  <a href="{{ route('logistik.dashboard') }}" class="pc-link">
+    <span class="pc-micon">
+      <i class="ph-duotone ph-gauge"></i>
+    </span>
+    <span class="pc-mtext" data-i18n="Dashboard">Dashboard</span>
+  </a>
+</li>
+<li class="pc-item pc-caption">
+  <label data-i18n="Manajemen Logistik">Manajemen Logistik</label>
+</li>
+
+
+
+<li class="pc-item">
+  <a href="{{ route('logistik.pengajuan-barang.index') }}" class="pc-link">
+    <span class="pc-micon">
+      <i class="ph-duotone ph-shopping-cart"></i>
+    </span>
+    <span class="pc-mtext" data-i18n="Pengajuan Barang">Pengajuan Barang</span>
+  </a>
+</li>
+<li class="pc-item">
+  <a href="{{ route('logistik.pengajuan-barang.create') }}" class="pc-link">
+    <span class="pc-micon">
+      <i class="ph-duotone ph-plus-circle"></i>
+    </span>
+    <span class="pc-mtext" data-i18n="Ajukan Barang Baru">Ajukan Barang Baru</span>
+  </a>
+</li>
