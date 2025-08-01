434dbb364d88f2f2d5943ec2d65716465aa181d5
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-08-02 04:40:45 +0700

    Upload app/Models/Cuti.php

---
+++ app/Models/Cuti.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cuti extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'karyawan_id',
+        'tanggal_mulai',
+        'tanggal_selesai',
+        'jenis_cuti',
+        'status',
+        'alasan',
+        'document_template_used',
+        'document_generated_at',
+        'form_data',
+    ];
+
+    protected $casts = [
+        'tanggal_mulai' => 'date',
+        'tanggal_selesai' => 'date',
+        'jenis_cuti' => \App\Enums\JenisCutiEnum::class,
+        'status' => \App\Enums\StatusCutiEnum::class,
+    ];
+
+    public function karyawan()
+    {
+        return $this->belongsTo(Karyawan::class, 'karyawan_id');
+    }
+}
