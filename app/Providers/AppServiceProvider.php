fcaece9701b530a02ab5b112b5800e9f94eebdd4
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-08-02 04:41:33 +0700

    Upload app/Providers/AppServiceProvider.php

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\View;
+use App\Models\Setting;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Schema::defaultStringLength(191);
+
+        View::composer('*', function ($view) {
+            $website_name = Setting::where('key', 'website_name')->first();
+            $view->with('website_name', $website_name);
+        });
+    }
+}
