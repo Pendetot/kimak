1eeac58481ba6bbcdaf9364aff851f3e60b00c28
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-08-02 04:44:39 +0700

    Upload resources/views/hrd/kpi_penilaian/index.blade.php

---
+++ resources/views/hrd/kpi_penilaian/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('title', 'Penilaian KPI')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Penilaian KPI Karyawan</h3>
+                </div>
+                <div class="card-body">
+                    <p>Halaman untuk melakukan penilaian KPI karyawan.</p>
+                    {{-- Add your KPI assessment form/table here --}}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
