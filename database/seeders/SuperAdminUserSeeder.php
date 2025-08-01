09300b9d379250aaa7458c3a61e7010c817cd32c
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-08-02 04:43:24 +0700

    Upload database/seeders/SuperAdminUserSeeder.php

---
+++ database/seeders/SuperAdminUserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Enums\RoleEnum;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class SuperAdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Super Admin',
+            'email' => 'superadmin@example.com',
+            'password' => Hash::make('password'),
+            'role' => RoleEnum::SuperAdmin,
+        ]);
+    }
+}
