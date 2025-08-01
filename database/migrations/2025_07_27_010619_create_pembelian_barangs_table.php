6aa0843570f7424f0b95c70d9efc837c97f09d06
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-08-02 04:42:39 +0700

    Upload database/migrations/2025_07_27_010619_create_pembelian_barangs_table.php

---
+++ database/migrations/2025_07_27_010619_create_pembelian_barangs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pembelian_barangs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pengajuan_barang_id')->constrained('pengajuan_barangs');
+            $table->string('nama_barang');
+            $table->integer('jumlah');
+            $table->decimal('harga_satuan', 15, 2);
+            $table->decimal('total_harga', 15, 2);
+            $table->date('tanggal_pembelian');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pembelian_barangs');
+    }
+};
