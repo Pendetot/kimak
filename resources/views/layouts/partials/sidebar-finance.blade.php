0d296b78969bddda147cf9d90047be177500f4c1
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-08-02 04:46:58 +0700

    Upload resources/views/layouts/partials/sidebar-finance.blade.php

---
+++ resources/views/layouts/partials/sidebar-finance.blade.php
@@ -0,0 +1,23 @@
+<li class="pc-item pc-caption">
+  <label data-i18n="Navigasi">Navigasi</label>
+  <i class="ph-duotone ph-gauge"></i>
+</li>
+<li class="pc-item">
+  <a href="{{ route('keuangan.dashboard') }}" class="pc-link">
+    <span class="pc-micon">
+      <i class="ph-duotone ph-gauge"></i>
+    </span>
+    <span class="pc-mtext" data-i18n="Dashboard">Dashboard</span>
+  </a>
+</li>
+<li class="pc-item pc-caption">
+  <label data-i18n="Manajemen Keuangan">Manajemen Keuangan</label>
+</li>
+<li class="pc-item">
+  <a href="{{ route('keuangan.hutang-karyawans.index') }}" class="pc-link">
+    <span class="pc-micon">
+      <i class="ph-duotone ph-wallet"></i>
+    </span>
+    <span class="pc-mtext" data-i18n="Hutang Karyawan">Hutang Karyawan</span>
+  </a>
+</li>
